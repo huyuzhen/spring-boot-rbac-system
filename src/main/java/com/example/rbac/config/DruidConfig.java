e7510fb59f6951b707bfe818726d80e6cdb61ef9
huyuzhen <745320353@qq.com>
2026-05-29 15:10:12 +0800

    feat: Add DruidConfig

---
+++ src/main/java/com/example/rbac/config/DruidConfig.java
@@ -0,0 +1,21 @@
+package com.example.rbac.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * Druid Connection Pool Configuration
+ */
+@Configuration
+public class DruidConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.druid")
+    public DataSource druidDataSource() {
+        return new DruidDataSource();
+    }
+}
